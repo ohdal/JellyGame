f3a6fb8dc9a3b83df98af2155c16f9e8af8c17e1
윤현정 <yoonhj97@naver.com>
2023-01-17 02:46:20 +0900

    [MOD] nodeJS(15.10.0) 버전따라 node-sass 버전 변경 & 필요없는 코드 수정

--- src/JellyGame.jsx
+++ src/JellyGame.jsx
@@ -12,8 +12,6 @@ import './JellyGame.scss'
 const jellyList = [jelly1, jelly2, jelly3, jelly4, jelly5, jelly6]
 
 const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
