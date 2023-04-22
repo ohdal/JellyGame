71cdcad943cf21528bf40583d346c8b2c796ee0e
윤현정 <yoonhj97@naver.com>
2023-04-22 18:25:23 +0900

    [ADD] momobile 기기로 접속시 MobilePage.jsx 띄우기

--- src/App.js
+++ src/App.js
@@ -3,20 +3,48 @@ import { Switch, Route } from 'react-router-dom';
 import { ModalContainer } from './containers/ModalContainer';
 import MainPage from './pages/MainPage';
 import GamePage from './pages/GamePage';
+import MobilePage from './pages/MobilePage';
+
+
+// mobile 기기 체크
+function detectMobileDevice(agent) {
+  const mobileRegex = [
+    /Android/i,
+    /iPhone/i,
+    // /iPad/i,
+    // /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i
+  ]
+  return mobileRegex.some(mobile => agent.match(mobile));
+}
+
+const isMobile = detectMobileDevice(window.navigator.userAgent);
+
+// 배포시 console.log & console.warn 지우기
+if (process.env.NODE_ENV === "production") {
+  console.log = function no_console() { };
+  console.warn = function no_console() { };
+}
 
 function App() {
   return (
     <div className="App">
-      <ModalContainer>
-        <Switch>
-          <Route exact path="/">
-            <MainPage />
-          </Route>
-          <Route exact path="/play">
-            <GamePage />
-          </Route>
-        </Switch>
-      </ModalContainer>
+      {
+        isMobile ?
+          <MobilePage /> :
+          <ModalContainer>
+            <Switch>
+              <Route exact path="/">
+                <MainPage />
+              </Route>
+              <Route exact path="/play">
+                <GamePage />
+              </Route>
+            </Switch>
+          </ModalContainer>
+
+      }
     </div>
   );
 }
---
+++ src/pages/MobilePage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import jellyImg3 from '../assets/images/jelly_type_3.png'
+import jellyImg5 from '../assets/images/jelly_type_5.png'
+import jellyImg6 from '../assets/images/jelly_type_6.png'
+
+const Wrapper = styled.div`
+  font-size: 1.25rem;
+  color: #FFFFFF;
+  margin: auto 0;
+`
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 10px;
+`
+
+export default function MobilePage() {
+  return (
+    <Wrapper>
+      <ImageContainer>
+        <img src={jellyImg3} alt="" width="50vw" height="50vh" />
+        <img src={jellyImg5} alt="" width="50vw" height="50vh" />
+        <img src={jellyImg6} alt="" width="50vw" height="50vh" />
+      </ImageContainer>
+      Mobile 버전은 이용하실 수 없습니다.  <br />
+      PC로 접속해주세요. 🥺
+    </Wrapper>
+  )
+}
